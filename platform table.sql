48c5bb47fcc7ea1b66ce8fbc27cc514cfe0bdc06
Hiroki879 <hiroki.nagaoka@beaconcom.co.jp>
2025-06-11 17:52:05 +0900

    Add files via upload

---
+++ platform table.sql
@@ -0,0 +1,39 @@
+CREATE TABLE "FunnelIO_platform_OLV" (
+  "date" date,
+  "imps" integer,
+  "clicks" integer,
+  "XXX" integer
+);
+
+CREATE TABLE "FunnelIO_platform_Social" (
+  "date" date,
+  "imps" integer,
+  "clicks" integer,
+  "XXXX" integer
+);
+
+CREATE TABLE "FunnelIO_platform_Display" (
+  "date" date,
+  "imps" integer,
+  "clicks" integer,
+  "XXXX" integer
+);
+
+CREATE TABLE "FunnelIO_platform_Managed" (
+  "date" date,
+  "imps" integer,
+  "clicks" integer,
+  "XXXX" integer
+);
+
+CREATE TABLE "JPCgenTracker" (
+  "program_id" integer,
+  "activity_Id" integer,
+  "activity_name" varchar,
+  "segment_name" varchar,
+  "product_name" varchar,
+  "platform_name" varchar,
+  "ddom_name" varchar,
+  "campaign_name" varchar
+);
+
